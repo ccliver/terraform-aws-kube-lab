8dbc599f78d5fe563fad571dc94d74ce4f99cd7a
Carl Cliver <ccliver@gmail.com>
2022-09-20 20:40:30 -0400

    fix: removing static ssh key from example

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -1,3 +1,8 @@
+variable "ssh_public_key" {
+  description = "Your SSH public key to be used to create the EC2 Key Pair for the instances"
+  type        = string
+}
+
 variable "ssh_allowed_cidrs" {
   description = "A list of CIDRs granted SSH access to the control plane and worker nodes"
   type        = list(any)
